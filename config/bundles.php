7774e208f56435959c470d4922ea7fdbce688259
Chaïma BEN YOUNES <benyounes@brocelia.fr>
2019-06-03 13:45:16 +0200

    RM #88632 upgrade to SF4, initiatlization/configuration of the BO with easyAdmin and add Header & News entities

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,8 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
