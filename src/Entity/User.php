abc6715460a8163a85756af33873f2dd41e831b2
Guillaume Marceteau <guillaumemarceteau+brocelia@gmail.com>
2019-06-14 14:33:08 +0200

    RM #88645 Add the authentication endpoint for the API

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -146,7 +146,7 @@ class User implements UserInterface
      */
     public function getPassword()
     {
-        // not needed for apps that do not check user passwords
+        return $this->password;
     }
 
     /**
