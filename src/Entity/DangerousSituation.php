37df111776dca0246c8652fe601a4a5111eb71f8
Chaïma BEN YOUNES <benyounes@brocelia.fr>
2019-06-28 09:48:31 +0200

    RM #88639 create Endpoint Dangerous Situation

---
+++ src/Entity/DangerousSituation.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DangerousSituationRepository")
+ */
+class DangerousSituation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\TypeDangerousSituation", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $typeDangerousSituation;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $photo;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Direction", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $direction;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Area", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $zone;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Entity", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $entity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getDirection(): ?Direction
+    {
+        return $this->direction;
+    }
+
+    public function setDirection(?Direction $direction): self
+    {
+        $this->direction = $direction;
+
+        return $this;
+    }
+
+    public function getZone(): ?Area
+    {
+        return $this->zone;
+    }
+
+    public function setZone(?Area $zone): self
+    {
+        $this->zone = $zone;
+
+        return $this;
+    }
+
+    public function getEntity(): ?Entity
+    {
+        return $this->entity;
+    }
+
+    public function setEntity(?Entity $entity): self
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    public function getTypeDangerousSituation(): ?TypeDangerousSituation
+    {
+        return $this->typeDangerousSituation;
+    }
+
+    public function setTypeDangerousSituation(?TypeDangerousSituation $typeDangerousSituation): self
+    {
+        $this->typeDangerousSituation = $typeDangerousSituation;
+
+        return $this;
+    }
+}
