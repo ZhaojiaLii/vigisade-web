3970c47925e00fcd873ebe11a913dbb9d35a2e6b
jmal <malaquin@brocelia.fr>
2019-07-17 16:17:29 +0200

    RM #90130 : API create/update user

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,11 +3,53 @@
 namespace App\Controller\Api;
 
 use App\Controller\ApiController;
+use App\Entity\User;
+use App\Exception\Http\NotFoundException;
+use App\Repository\AreaRepository;
+use App\Repository\DirectionRepository;
+use App\Repository\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class UserController extends ApiController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var DirectionRepository
+     */
+    private $directionRepository;
+
+    /**
+     * @var AreaRepository
+     */
+    private $areaRepository;
+
+    /**
+     * @var EntityRepository
+     */
+    private $entityRepository;
+
+
+    public function __construct(
+        EntityManagerInterface $em,
+        DirectionRepository $directionRepository,
+        AreaRepository $areaRepository,
+        EntityRepository $entityRepository
+    ) {
+        $this->em = $em;
+        $this->directionRepository = $directionRepository;
+        $this->areaRepository = $areaRepository;
+        $this->entityRepository = $entityRepository;
+    }
+
     /**
      * Gets information about the current user.
      */
@@ -35,44 +77,63 @@ class UserController extends ApiController
     }
 
     /**
-     * Updates information of the current user.
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function updateUser()
+    public function updateUser(Request $request)
     {
-        /*
-         * @todo: checks payload data are correct.
-         *      - App\Service\Validator\ApiPayload\UserValidator
-         *      - App\Repository\UserRepository
-         */
+        $data = json_decode($request->getContent(), true);
 
-        /*
-         * @todo: update the current user.
-         *      - App\Repository\UserRepository
-         */
+        if (empty($data)) {
+            throw new NotFoundException("Data empty");
+        }
 
-        /*
-         * @todo: returns the current user.
-         */
+        $user = ApiController::getUser();
 
-        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        $direction = array_key_exists('direction_id', $data) ? $this->directionRepository->find($data['direction_id']) : null;
+        $area = array_key_exists('area_id', $data) ? $this->areaRepository->find($data['area_id']) : null;
+        $entity = array_key_exists('entity_id', $data) ? $this->entityRepository->find($data['entity_id']) : null;
+
+        $user->setFirstname($data['firstname']);
+        $user->setLastname($data['lastname']);
+        $user->setDirection($direction);
+        $user->setArea($area);
+        $user->setEntity($entity);
+        $user->setImage($data['image']);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return new JsonResponse([
+            'id' => $user->getId(),
+            'direction' => $direction ? $direction->getId() : null,
+            'area' => $area ? $area->getId() : null,
+            'entity' => $entity ? $entity->getId() : null
+
+        ], 200);
     }
 
-    public function createUser()
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createUser(Request $request)
     {
-        /*
-         * @todo: checks payload data are correct.
-         *      - App\Service\Validator\ApiPayload\UserValidator
-         */
+        $data = json_decode($request->getContent(), true);
 
-        /*
-         * @todo: create a new user.
-         *      - App\Repository\UserRepository
-         */
+        if (empty($data)) {
+            throw new NotFoundException("data empty");
+        }
 
-        /*
-         * @todo: returns the created user.
-         */
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setfirstname($data['firstname']);
+        $user->setPassword($data['password']);
+        $user->setImage($data['image']);
+        $user->setActif(true);
+        $this->em->persist($user);
+        $this->em->flush();
 
-        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        return new JsonResponse(null, 200);
     }
 }
