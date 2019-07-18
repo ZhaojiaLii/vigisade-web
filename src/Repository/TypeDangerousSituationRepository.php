d75933ca7c4fde7a73ab0240eed3088083234f28
Chaïma BEN YOUNES <benyounes@brocelia.fr>
2019-07-18 11:18:49 +0200

    RM #90157 add endpoint get type situation dangerouses

--- src/Repository/TypeDangerousSituationRepository.php
+++ src/Repository/TypeDangerousSituationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\TypeDangerousSituation;
+use App\Exception\Http\NotFoundException;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -14,8 +15,32 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class TypeDangerousSituationRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    private $typeDangerousSituationTranslationRepository;
+
+    public function __construct(
+        RegistryInterface $registry,
+        TypeDangerousSituationTranslationRepository $typeDangerousSituationTranslationRepository )
     {
         parent::__construct($registry, TypeDangerousSituation::class);
+        $this->typeDangerousSituationTranslationRepository = $typeDangerousSituationTranslationRepository;
+    }
+
+    public function getAllTypeDangerousSituation()
+    {
+        $typeDangerousSituations = $this->findAll();
+
+        if (!$typeDangerousSituations) {
+            throw new NotFoundException(" Type of Dangerous Situations is empty ");
+        }
+
+        foreach ($typeDangerousSituations as $typeDangerousSituation ){
+
+            $responseArray []  = [
+                "typeDangerousSituationsId" => $typeDangerousSituation->getId(),
+                "typeDangerousSituationTranslation" => $this->typeDangerousSituationTranslationRepository->getAllTypeDangerousSituationTranslation($typeDangerousSituation->getId())
+            ];
+        }
+
+        return $responseArray;
     }
 }
