dd2b5c93a74bf308e6afdf7aeb019314948e617f
Sébastien FAYE <faye@brocelia.fr>
2019-07-22 14:59:17 +0200

    Update EntityFixtures.php

--- src/DataFixtures/EntityFixtures.php
+++ src/DataFixtures/EntityFixtures.php
@@ -9,7 +9,7 @@ class EntityFixtures extends BaseFixture
 {
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(30, 'main_entity', function($i) use ($manager) {
+        $this->createMany(3, 'main_entity', function($i) use ($manager) {
             $entity = new Entity();
             $entity->setName($this->faker->state);
             $entity->setEtat(true);
