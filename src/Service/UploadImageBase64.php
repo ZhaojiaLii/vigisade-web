4f1f911e5910d9e3441781edda67c6ce481ebc0a
Chaïma BEN YOUNES <benyounes@brocelia.fr>
2019-07-30 13:21:43 +0200

    new service to store image base64

---
+++ src/Service/UploadImageBase64.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service;
+
+class UploadImageBase64
+{
+    public function UploadImage($picture, $dir){
+        if(preg_match("#^data:image\/(?<extension>(?:png|gif|jpg|jpeg));base64,(?<image>.+)$#", $picture, $matchings)){
+            $extension = $matchings['extension'];
+            $imageData = base64_decode($matchings['image']);
+            //$data = base64_decode($picture);
+            $basename=uniqid();
+            $file = $dir . $basename .'.'.$extension;
+
+            if (file_put_contents($file, $imageData)) {
+                return $basename.'.'.$extension;
+            } else {
+                return false;
+            }
+        }
+    }
+}
