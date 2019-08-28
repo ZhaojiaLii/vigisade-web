b45ae6c8afafbe68b43f21701942dec166c90abe
jmal <malaquin@brocelia.fr>
2019-08-28 16:33:56 +0200

    RM #93443 : survey - mailsAlertCorrectiveAction added

--- src/Entity/Survey.php
+++ src/Entity/Survey.php
@@ -54,6 +54,11 @@ class Survey
     private $direction;
 
     /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $mailsAlertCorrectiveAction;
+
+    /**
      * @param $method
      * @param $arguments
      * @return mixed
@@ -222,5 +227,21 @@ class Survey
         }
 
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMailsAlertCorrectiveAction()
+    {
+        return $this->mailsAlertCorrectiveAction;
+    }
+
+    /**
+     * @param mixed $mailsAlertCorrectiveAction
+     */
+    public function setMailsAlertCorrectiveAction($mailsAlertCorrectiveAction): void
+    {
+        $this->mailsAlertCorrectiveAction = $mailsAlertCorrectiveAction;
     }
 }
