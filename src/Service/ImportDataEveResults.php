9d0ee8ae15e783217662b93e63d6593caafa99e3
jmal <malaquin@brocelia.fr>
2019-10-01 15:27:14 +0200

    RM #89960 : fix import result

--- src/Service/ImportDataEveResults.php
+++ src/Service/ImportDataEveResults.php
@@ -7,6 +7,8 @@ use App\Entity\Direction;
 use App\Entity\Result;
 use App\Entity\ResultQuestion;
 use App\Entity\ResultTeamMember;
+use App\Entity\SurveyQuestion;
+use App\Entity\SurveyQuestionTranslation;
 use App\Entity\User;
 use App\Repository\DirectionRepository;
 use App\Repository\ResultRepository;
@@ -34,25 +36,41 @@ class ImportDataEveResults
      * @return mixed[]
      * @throws \Doctrine\DBAL\DBALException
      */
-    protected function getEveData()
+    protected function getResultEve()
     {
         $sql = 'SELECT 
+c.id as controle_id,
 c.controleur as controle_controleur,
 c.date_controle as controle_date,
 c.chantier as controle_chantier,
 c.nom_entreprise as controle_nom_entreprise,
+c.commentaire as controle_commentaire,
 c.etat as controle_etat,
 c.ok as controle_ok,
 c.ko as controle_ko,
 c.nv as controle_nv,
 c.ac as controle_ac,
-c.commentaire as controle_commentaire,
-i.nom as equipe_nom,
+q.nom as question_nom,
 i.prenom as equipe_prenom,
 p.nom as pj_nom
 FROM eve_securite_controle c
+JOIN eve_interne_question q ON c.id_instance = q.id
 JOIN eve_interne_equipe i ON i.id_controle = c.id
 JOIN eve_pj_interne_controle p ON p.id_controle = c.id';
+
+        $conn = $this->em->getConnection();
+        $data = $conn->prepare($sql);
+        $data->execute();
+        return $data->fetchAll();
+    }
+
+    /**
+     * @return mixed[]
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getSuiviNcEveData() {
+        $sql = 'SELECT *
+FROM eve_suivi_nc';
 
         $conn = $this->em->getConnection();
         $data = $conn->prepare($sql);
@@ -91,7 +109,7 @@ JOIN eve_pj_interne_controle p ON p.id_controle = c.id';
 
         $this->em->flush();
 
-        $resultsData = $this->getEveData();
+        $resultsData = $this->getResultEve();
 
 
         $output->writeln([
@@ -128,53 +146,78 @@ JOIN eve_pj_interne_controle p ON p.id_controle = c.id';
                 $result->setDirection($user->getDirection());
                 $result->setArea($user->getArea());
                 $result->setEntity($user->getEntity());
+
+                $result->setDate(new \DateTime($value['controle_date']));
+                $result->setPlace($value['controle_chantier']);
+                $result->setClient($value['controle_nom_entreprise']);
+                $result->setValidated($value['controle_etat']);
+                $result->setBestPracticeDone(2);
+                $this->manager->persist($result);
+                $this->manager->flush();
+
+
+                // Result Question
+                $questionTranslation = $this->manager
+                    ->getRepository(SurveyQuestionTranslation::class)
+                    ->findOneBy(['label' => $value['question_nom']]);
+
+                if ($questionTranslation) {
+
+                    $resultQuestion = new ResultQuestion();
+                    $resultQuestion->setResult($result);
+                    $resultQuestion->setQuestion($questionTranslation->getTranslatable());
+                    $resultQuestion->setNotation(1);
+                    $resultQuestion->setComment($value['controle_commentaire']);
+                    $resultQuestion->setPhoto($value['pj_nom']);
+                    $this->manager->persist($resultQuestion);
+                    $this->manager->flush();
+
+                    // Result Team Member
+                    $resultTeamMember = new ResultTeamMember();
+                    $resultTeamMember->setResult($result);
+                    $resultTeamMember->setFirstName($controleur[0]);
+                    $resultTeamMember->setLastName($controleur[1]);
+                    $resultTeamMember->setRole('');
+                    $this->manager->persist($resultTeamMember);
+                    $this->manager->flush();
+                }
+
+
+                $progressBar->advance();
+                echo "Result Id " . $result->getId();
             }
-            $result->setDate(new \DateTime($value['controle_date']));
-            $result->setPlace($value['controle_chantier']);
-            $result->setClient($value['controle_nom_entreprise']);
-            $result->setValidated($value['controle_etat']);
-            $result->setBestPracticeDone(2);
-            $this->manager->persist($result);
-            $this->manager->flush();
 
-            // Result Question
-            $resultQuestion = new ResultQuestion();
-            $resultQuestion->setResult($result);
-            $resultQuestion->setQuestion(1);
-            $resultQuestion->setNotation(1);
-            $resultQuestion->setComment($value['controle_commentaire']);
-            $resultQuestion->setPhoto($value['pj_nom']);
-            $this->manager->persist($resultQuestion);
-            $this->manager->flush();
+        }
+        $suiviNcData = $this->getSuiviNcEveData();
 
+        foreach ($suiviNcData as $suiviNc) {
+            if ($suiviNc['acteur']) {
+                $acteur = explode(' ', $suiviNc['acteur']);
 
-            // Result Team Member
-            $resultTeamMember = new ResultTeamMember();
-            $resultTeamMember->setResult($result);
-            $resultTeamMember->setFirstName($controleur[0]);
-            $resultTeamMember->setLastName($controleur[1]);
-            $resultTeamMember->setRole('');
-            $this->manager->persist($resultTeamMember);
-            $this->manager->flush();
+                $user = $this->manager
+                    ->getRepository(User::class)
+                    ->findOneBy(['lastname' => $acteur[0], 'firstname' => $acteur[1]]);
 
-            // Corrective Action
-            $correctiveAction = new CorrectiveAction();
-            $correctiveAction->setUser($user);
-            $correctiveAction->setQuestion(1);
-            $correctiveAction->setResult($result);
-            $correctiveAction->setResultQuestion($resultQuestion);
-            $correctiveAction->setDirection($result->getDirection());
-            $correctiveAction->setArea($result->getArea());
-            $correctiveAction->setEntity($result->getEntity());
-            $correctiveAction->setStatus("Validé");
-            $correctiveAction->setImage(null);
-            $correctiveAction->setCommentQuestion('action_realisée');
-            $this->manager->persist($correctiveAction);
-            $this->manager->flush();
+                if ($user) {
+                    // Corrective Action
+                    $correctiveAction = new CorrectiveAction();
+                    $correctiveAction->setUser($user);
+                    $correctiveAction->setDirection($user->getDirection());
+                    $correctiveAction->setArea($user->getArea());
+                    $correctiveAction->setEntity($user->getEntity());
 
+                    if ($suiviNc['action_realise'] == 'Oui') {
+                        $correctiveAction->setStatus("Validé");
+                    } else {
+                        $correctiveAction->setStatus("A traiter");
+                    }
 
-            $progressBar->advance();
-            echo "Result Id " . $result->getId();
+                    $correctiveAction->setImage($value['pj_nom']);
+                    $this->manager->persist($correctiveAction);
+                    $this->manager->flush();
+                }
+            }
+
         }
 
         $progressBar->finish();
